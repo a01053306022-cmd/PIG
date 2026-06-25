84a32ddc41c8ff289db037a946d94b5951752427
https://github.com/ekdbss/pokemon-battle-simulator.git <i4302904@gmail.com>
2026-06-25 14:25:36 +0900

    파이프라인 삭제

--- app.py
+++ app.py
@@ -180,9 +180,7 @@ def login():
         is_overseas=bool(is_foreign),
         is_new_device=is_new_device,
         fail_count=login_attempts[ip],
-        is_odd_time=is_odd_time,  
-        pipeline_result.get('risk_score', 0.0), 
-        pipeline_result.get('status', '정상')            
+        is_odd_time=is_odd_time,        
     )
 
     # real_dataset 테이블에 로그인 기록 저장
